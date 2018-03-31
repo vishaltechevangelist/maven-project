22cabad927b303ea9ab4cc1446f8fc5e18586614
Vishal Saxena <vishal.saxena@naukri.com>
2018-03-31 17:51:12 +0530

    fixing checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * 
+   * @param someone is the name
+   * @return greet someone
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This is package info example
+ */
+package com.example;
